fd315d54facf705c6421ed38206c86648741fe49
Raiane-Dev <raiane.dev@gmail.com>
2022-11-30 16:47:48 -0300

    front

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -1,8 +1,37 @@
-import { Row, Layout, Typography, Button } from 'antd';
-import { HeatMapOutlined, PlusOutlined } from '@ant-design/icons';
+import { useState, useRef } from 'react';
+import { Row, Layout, Typography, Avatar, List, Divider, Space, Button, Switch  } from 'antd';
+import { HeatMapOutlined, UsergroupAddOutlined } from '@ant-design/icons';
+import github from "../assets/images/github.png"
+import linkedin from "../assets/images/linkedin.png"
+import instagram from "../assets/images/instagram.webp"
+import twitter from "../assets/images/twitter.png"
 
 const { Footer } = Layout;
-const { Title, Paragraph } = Typography;
+const { Title, Link } = Typography;
+
+const data = [
+    {
+      title: 'Github.com',
+      image: github,
+      link: "https://github.com/Raiane-Dev"
+    },
+    {
+      title: 'Linkedin',
+      image: linkedin,
+      link: "https://www.linkedin.com/in/raiane-dev"
+    },
+    {
+      title: 'Instagram',
+      image: instagram,
+      link: "https://www.instagram.com/raiane_daros"
+    },
+    {
+      title: 'Twitter',
+      image: twitter,
+      link: "https://twitter.com/RaianeDev"
+    },
+  ];
+
 
 function _Footer(): any
 {
@@ -12,15 +41,44 @@ function _Footer(): any
                 <Row className='box'>
                     <Row>
                         <Title level={4}>
-                            Let's Have a Fun!
+                            People who to follow. 
+                            <UsergroupAddOutlined />
                         </Title>
-                        <Paragraph>
-                            Like a nice party to this night? <br /> Let's Go!
-                        </Paragraph>
+                        <List
+                            itemLayout="horizontal"
+                            dataSource={data}
+                            renderItem={(item) => (
+                            <List.Item>
+                                <List.Item.Meta
+                                avatar={<Avatar size="small" src={item.image} />}
+                                title={<a className='link-href' href={item.link}>{item.title}</a>}
+                                />
+                            </List.Item>
+                            )}
+                        />
                     </Row>
-                    <Row className='icon'>
-                        <HeatMapOutlined />
-                    </Row>
+                </Row>
+
+                <Row className='icon'>
+                    <Divider />
+                    <Title level={4}>
+                        People who to follow. 
+                        <UsergroupAddOutlined />
+                    </Title>
+
+                    <List
+                        itemLayout="horizontal"
+                        dataSource={data}
+                        renderItem={(item) => (
+                          <List.Item>
+                            <List.Item.Meta
+                              title={<a href="https://ant.design">{item.title}</a>}
+                              description="03 ago"
+                            />
+                          </List.Item>
+                        )}
+                      />
+
                 </Row>
             </Footer>
         </>
--- Frontend/src/views/Home.tsx
+++ Frontend/src/views/Home.tsx
@@ -1,5 +1,5 @@
 import { SearchOutlined, VerticalLeftOutlined } from '@ant-design/icons';
-import { Image, Layout, Col, Typography, Row, Button, Card } from 'antd';
+import { Image, Layout, Col, Typography, Row, Button, Card, Divider } from 'antd';
 import walp from "../assets/images/walp.jpg"
 import _Navbar from '../components/Navbar';
 
@@ -36,7 +36,58 @@ function Home(): any {
                 </Col>
               </Row>
             </Card>
+
+            <Card className='box'>
+              <Row>
+                <Col span={12}>
+                  <Image src={walp} preview={false} />
+                </Col>
+                <Col span={12}>
+                  <Title level={4}>
+                    Monster
+                  </Title>
+                  <Paragraph className='content'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod egestas faucibus. Maecenas lacinia enim vel risus pellentesque, ut ullamcorper odio vestibulum. Vestibulum quis gravida sapien. Curabitur sollicitudin pharetra risus, vitae placerat elit posuere eu. Cras nec velit felis...
+                  </Paragraph>
+        
+                  <Paragraph className='date'>
+                    3 ago
+                  </Paragraph>
+                  <Col className='icon-act'>
+                    <VerticalLeftOutlined />
+                  </Col>
+                </Col>
+              </Row>
+            </Card>
           
+          </Row>
+
+          <Divider />
+          <Row className='list-items'>
+
+          <Card className='box'>
+              <Row>
+                <Col span={8}>
+                  <Image src={walp} preview={false} />
+                </Col>
+                <Col span={16}>
+                  <Title level={4}>
+                    Monster
+                  </Title>
+                  <Paragraph className='content'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod egestas faucibus. Maecenas lacinia enim vel risus pellentesque, ut ullamcorper odio vestibulum. Vestibulum quis gravida sapien. Curabitur sollicitudin pharetra risus, vitae placerat elit posuere eu. Cras nec velit felis...
+                  </Paragraph>
+        
+                  <Paragraph className='date'>
+                    3 ago
+                  </Paragraph>
+                  <Col className='icon-act'>
+                    <VerticalLeftOutlined />
+                  </Col>
+                </Col>
+              </Row>
+            </Card>
+
           </Row>
         </Content>
       </Layout>
